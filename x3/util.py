b32867e227592a21757df8ae197dc2686af0e969
xhm <525150210@qq.com>
2019-02-01 11:28:15 +0800

    增加x3的pickle功能

--- x3/util.py
+++ x3/util.py
@@ -1,4 +1,3 @@
-__doc__ = r"""
 #
 #                                                    ==(W{==========-      /===-
 #                                                      ||  (.--.)         /===-_---~~~~~~~~~------____
@@ -46,7 +45,7 @@ __doc__ = r"""
 #              |-|                         '^`   (o  o)  '^`                          | |
 #              | |                               `\VV/'                               |-|
 #              |-|                                                                    | |
-#              | |       Create on 26/11/2018  All rights reserved by ZIJIAN JIANG    |-|
+#              | |       Create on 01/02/2019  All rights reserved by ZIJIAN JIANG    |-|
 #              |-|                                                                    | |
 #              | |                                                                    |-|
 #              |_|____________________________________________________________________| |
@@ -62,8 +61,8 @@ __doc__ = r"""
 # ██║▄▄ ██║██║   ██║██║ ██╔██╗ ██║   ██║   ██║   ██╔══╝      ██╔██╗  ╚═══██╗
 # ╚██████╔╝╚██████╔╝██║██╔╝ ██╗╚██████╔╝   ██║   ███████╗██╗██╔╝ ██╗██████╔╝
 #  ╚══▀▀═╝  ╚═════╝ ╚═╝╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚══════╝╚═╝╚═╝  ╚═╝╚═════╝ 
-"""
-# Copyright (C) 2018  ZIJIAN JIANG
+
+# Copyright (C) 2019  ZIJIAN JIANG
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -78,165 +77,120 @@ __doc__ = r"""
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import sys
-import itertools
-from typing import List, Optional, Tuple, Any, Union, cast
+
+import gc
+from typing import Hashable, Any, List, Dict, Optional, Deque
 from lxml import etree
-from collections import Sequence
-from util import xml2tree, xml2file, XmlFilePuller, XmlElemPuller
+from collections import deque
+from datetime import datetime
 
+# ==========================================
+#   Memory Utilities
+# ==========================================
+class LRUCacheItem:
+    """Data structure of items stored in cache"""
+    def __init__(self, key: Hashable, value: Any = None):
+        self.__key = key
+        self.value = value or key
+        self.timestamp = datetime.now()
 
-Xpath = str
-
-g_xmlpuller = XmlFilePuller(cachesize=1000)
-g_elempuller = XmlElemPuller(g_xmlpuller, cachesize=100000)
-
-
-class x3(Sequence):
-
-    @staticmethod
-    def setfilecachesize(size: int):
-        g_xmlpuller = XmlFilePuller(cachesize=size)
-
-    @staticmethod
-    def setelemcachesize(size: int):
-        g_elempuller = XmlElemPuller(g_xmlpuller, cachesize=size)
-
-    def __init__(self, elem: etree._Element):
-        self._elem = elem
-    
-    def selectall(self, selection: Xpath) -> 'x3_group':
-        return x3_group([x3(x) for x in self._elem.xpath(selection)])
-    
-    def select(self, selection: Xpath, default: Any = None) -> Optional['x3']:
-        child = next(iter(self._elem.xpath(selection)), default)
-        if child is not None:
-            return x3(child)
-        else:
-            return None
-        
-    def attrib(self, key, default=None) -> Optional[str]:
-        return self._elem.get(key, default)
-
-    def parent(self, selection: Optional[Xpath] = None) -> Optional['x3']:
-        if selection is not None:
-            return x3(next(self._elem.iterancestors(selection)))
-        else:
-            return x3(self._elem.getparent())
-
-    def remove(self):
-        self._elem.getparent().remove(self._elem)
-
-    def getsourcefile(self) -> Tuple[str, int]:
-        return (self._elem.base, self._elem.sourceline)
-
-    def __getitem__(self, key):
-        if type(key) is str:
-            return self._elem.attrib[key]
-        elif type(key) is int:
-            if key >= len(self):
-                raise IndexError("index out of range")
-            return x3(self._elem[key])
-        else:
-            raise TypeError("Key error")
-
-    def __setitem__(self, key: Union[str, int], item: Any):
-        if type(key) is str:
-            self._elem.attrib[key] = item
-        elif type(key) is int:
-            if cast(int, key) >= len(self):
-                raise IndexError("index out of range")
-            self._elem[key] = item
-        else:
-            raise TypeError("Key error")
-
-    def __len__(self) -> int:
-        return len(self._elem)
-
-    def __hash__(self):
-        return hash(self._elem)
-    
-    def __eq__(self, other):
-        if other is None and self._elem is None:
-            return True
-        if type(other) is not type(self):
-            return False
-        return self._elem is other._elem
-
-    def __bool__(self):
-        return self._elem != None
+    def __eq__(self, other) -> bool:
+        return self.key is other.key
 
     @property
-    def tag(self) -> str:
-        return self._elem.tag
-
-    # for pickle serialize
-    def __getstate__(self):
-        # Copy the object's state from self.__dict__ which contains
-        # all our instance attributes. Always use the dict.copy()
-        # method to avoid modifying the original state.
-        state = self.__dict__.copy()
-        # Remove the unpicklable entries.
-        
-        state["filename"], state["lineno"] = self.getsourcefile()
-        if sys.platform == "win32":
-            state["filename"] = state["filename"][len("file:/"):]
-        del state['_elem']
-        return state
-
-    def __setstate__(self, state):
-        # Restore instance attributes (i.e., filename and lineno).
-        self.__dict__.update(state)
-        # Restore the previously opened file's state. To do so, we need to
-        # reopen it and read from it until the line count is restored.
-        self._elem = g_elempuller.get(self.filename, self.lineno)
+    def key(self):
+        return self.__key
 
 
+class LRUCache:
+    """A sample class that implements LRU algorithm"""
+    def __init__(self, length: int):
+        self.__length: int = length
+        self.__hash: Dict[Hashable, Any] = {}
+        self.__item_list: Deque[LRUCacheItem] = deque()
 
-
-class x3_group(Sequence):
-    def __init__(self, group: List[x3]):
-        self._group: List[x3] = group
-
-    #TODO: what if selection is None
-    def select(self, selection: Xpath) -> 'x3_group':
-        group: List[x3] = list(filter(None.__ne__, iter(x.select(selection) for x in self._group)))
-        return x3_group(group)
-
-    def selectall(self, selection: Xpath) -> 'x3_group':
-        selects = [x.selectall(selection)._group for x in self._group]
-        selects_group = list(itertools.chain.from_iterable(selects))
-        return x3_group(selects_group)
-
-    def parent(self, selection) -> 'x3_group':
-        group: List[x3] = list(filter(None.__ne__, iter(x.parent(selection) for x in self._group)))
-        return x3_group(group)
-            
-        
-    def map(self, func) -> List[Any]:
-        """
-        In catagory theory map is transform into another catagory.. Out of x3 catagory
-        """
-        return [func(x3_elem) for x3_elem in self._group]
-
-    def remove(self):
-        for x in self._group:
-            x.remove()
-
-    def filter(self, func) -> 'x3_group':
-        return x3_group([x3_elem for x3_elem in self._group if func(x3_elem)])
-
-    def toarray(self) -> List[x3]:
-        return self._group
-
-    def __len__(self) -> int:
-        return len(self._group)
-
-    def __getitem__(self, index):
-        if type(index) is int:
-            return self._group[index]
+    def insert(self, item: LRUCacheItem):
+        if item.key in self.__hash:
+            self.rankup(item)
         else:
-            raise TypeError("index error")
+            if len(self.__item_list) == self.__length:
+                lru_item = self.__item_list.pop()
+                del self.__hash[lru_item.key]
+                gc.collect()
+            #!if
+            self.__hash[item.key] = item
+            self.__item_list.appendleft(item)
 
-    def __bool__(self):
-        return len(self._group) != 0
+    def rankup(self, item: LRUCacheItem):
+        item_index = self.__item_list.index(item)
+        del self.__item_list[item_index]
+        self.__item_list.appendleft(item)
+
+    def get(self, key: Hashable) -> Optional[LRUCacheItem]:
+        if key not in self.__hash:
+            return None
+        item = self.__hash[key]
+        self.rankup(item)
+        return item
+
+
+# ==========================================
+#   File Utilities
+# ==========================================
+
+def xml2tree(path: str) -> etree._Element:
+    with open(path, "r", encoding="utf-8") as file:
+        tree = etree.parse(file)
+        
+    root = tree.getroot()
+    if not etree.iselement(root):
+        raise Exception("error")
+    return tree
+
+def xml2file(root: etree._Element, path: str):
+    with open(path, mode='w') as file:
+        parser = etree.XMLParser(remove_blank_text=True)
+        string = str(etree.tostring(root, pretty_print=True, encoding='unicode'))
+        root = etree.XML(string, parser)
+        output = str(etree.tostring(root, pretty_print=True, encoding='utf-8', xml_declaration=True).decode('utf-8'))
+        file.write(output)
+
+class XmlFilePuller:
+    """Xml File Mannager for xml files and etrees and cache management
+    """
+    def __init__(self, *, cachesize: int):
+        # CacheDict[fpath, etree]
+        self.__lrucache: LRUCache = LRUCache(cachesize)
+        
+    def get(self, fpath: str) -> etree.ElementTree:
+        item = self.__lrucache.get(fpath)
+        if item:
+            return item.value
+
+        item = LRUCacheItem(fpath, xml2tree(fpath))
+        self.__lrucache.insert(item)
+        return item.value
+
+
+class XmlElemPuller:
+    """Xml Element Puller for elements with Guid quick finding
+    """
+    def __init__(self, xmlpuller: XmlFilePuller, *, cachesize: int):
+        self.__xmlpuller = xmlpuller
+
+        # CacheDict[fpath, Dict[lineno, etree._Element]]
+        self.__lrucache: LRUCache = LRUCache(cachesize)
+    
+    def get(self, fpath: str, lineno: int) -> Optional[etree._Element]:
+        item = self.__lrucache.get(fpath)
+        if item:
+            return item.value.get(lineno, None) # type: ignore
+        
+        lineno_elem_map = {}
+        for elem in self.__xmlpuller.get(fpath).getroot().iter():
+            elem_lineno: int = elem.sourceline
+            lineno_elem_map[elem_lineno] = elem
+        #!for
+        item = LRUCacheItem(fpath, lineno_elem_map)
+        self.__lrucache.insert(item)
+        return item.value.get(lineno, None) # type: ignore
